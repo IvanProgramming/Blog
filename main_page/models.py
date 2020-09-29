00a460ce17629f705c6faf08fff6db1c7685f41a
IvanProgramming <vlasov.ivan.2004@gmail.com>
2020-09-29 20:34:09 +0300

    ➕ Added posted_on field in Article

--- main_page/models.py
+++ main_page/models.py
@@ -10,5 +10,6 @@ class Articles(models.Model):
     title = models.CharField(max_length=192)
     description = models.CharField(max_length=1024)
     md_text = models.TextField()
+    posted_on = models.DateTimeField(auto_now=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
