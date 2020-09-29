edb1ff40163a9ea58d3b31ddcf114cf6789c4675
IvanProgramming <vlasov.ivan.2004@gmail.com>
2020-09-29 17:42:49 +0300

    📰 Added Articles and Category models

--- main_page/models.py
+++ main_page/models.py
@@ -1,1 +1,14 @@
-# Create your models here.
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=92)
+
+
+class Articles(models.Model):
+    title = models.CharField(max_length=192)
+    description = models.CharField(max_length=1024)
+    md_text = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
