0576f21165a66f0b05cf8d6e635c3688b40696c4
Ivan <vlasov.ivan.2004@gmail.com>
2020-10-02 10:13:11 +0300

    ➕ Added show_random realization

--- main_page/views.py
+++ main_page/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .models import Articles, Category
+import random
 
 
 def show_main_page(request):
@@ -14,3 +15,8 @@ def show_article(request, article_name):
     return render(request, "post.html", context={
         "article": Articles.objects.get(id=article_name)
     })
+
+
+def show_random(request):
+    r_article = random.choice(Articles.objects.all())
+    return redirect(f"/post/{r_article.id}")
