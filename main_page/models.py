5ff1042ea25c2b7484662ffedca38c8b7dfee865
Ivan <vlasov.ivan.2004@gmail.com>
2020-10-02 10:12:10 +0300

    🔃 Added __str__ magick methods

--- main_page/models.py
+++ main_page/models.py
@@ -5,6 +5,9 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=92)
 
+    def __str__(self):
+        return self.name
+
 
 class Articles(models.Model):
     title = models.CharField(max_length=192)
@@ -13,3 +16,6 @@ class Articles(models.Model):
     posted_on = models.DateTimeField(auto_now=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.title} by {self.author}"
